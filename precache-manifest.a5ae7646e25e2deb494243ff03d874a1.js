abc19957f4a95ddf9418102aa2f3a0e7aaf84dfe
Oscar <oscarbaden@gmail.com>
2021-04-01 01:14:54 +1300

    Updates

---
+++ precache-manifest.a5ae7646e25e2deb494243ff03d874a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd7c630a47f6694d4896c09085458334",
+    "url": "/Pathfinder/index.html"
+  },
+  {
+    "revision": "fc4ef08fd8ba598464cd",
+    "url": "/Pathfinder/static/css/main.f09bac95.chunk.css"
+  },
+  {
+    "revision": "1087df7b931cb4c43558",
+    "url": "/Pathfinder/static/js/2.7e7ecf70.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Pathfinder/static/js/2.7e7ecf70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc4ef08fd8ba598464cd",
+    "url": "/Pathfinder/static/js/main.dfffedd7.chunk.js"
+  },
+  {
+    "revision": "9a4f61739431b57953e4",
+    "url": "/Pathfinder/static/js/runtime-main.96ef5446.js"
+  }
+]);
